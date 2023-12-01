9489b7cb18678e88e6b63cabf60fe64a7f6ee33d
PETREMANN Marc <57889978+MPETREMANN11@users.noreply.github.com>
2023-12-01 16:43:36 +0100

    Add files via upload

--- utf8/main.fs
+++ utf8/main.fs
@@ -2,7 +2,7 @@
 \ main file for UT8 characters key and emit
 \    Filename:      ùain.fs
 \    Date:          29 nov 2023
-\    Updated:       29 nov 2023
+\    Updated:       01 dec 2023
 \    File Version:  1.1
 \    MCU:           Linux / Web / Windows
 \    Forth:         eForth Linux
@@ -15,7 +15,7 @@
 DEFINED? --xutf8  [IF]  forget --xutf8  [THEN]
 create --xutf8
 
-\ s" dumpTool.fs"     included
+s" dumpTool.fs"     included
 s" ukey.fs"         included
 s" uemit.fs"        included
 
--- utf8/uemit.fs
+++ utf8/uemit.fs
@@ -2,7 +2,7 @@
 \ emit word for UT8 characters
 \    Filename:      uemit.fs
 \    Date:          29 nov 2023
-\    Updated:       29 nov 2023
+\    Updated:       01 dec 2023
 \    File Version:  1.1
 \    MCU:           Linux / Web / Windows
 \    Forth:         eForth Linux
@@ -12,38 +12,27 @@
 \ *********************************************************************
 
 
-\ buffer for UTF8 char
-create charUTF8     
-   0 c,  0 c,  0 c,  0 c,  \ allot 4 bytes only
-
-: clearCharUTF8 ( -- )
-    charUTF8 4 0 fill
-  ;
-
 \ reverse interger bytes, example:
-\  hex 1a2b3C -- 3c2b1a
-: reverse-bytes-to-charUTF8  { n0 -- }
+\  hex 1a2b3C --> 3c2b1a
+: reverse-bytes  ( n0 -- )
     0 { result }
-    n0
-    begin
-        $100 u/mod
-    ?dup while
-        swap
-        result $100 * + to result
-    repeat
-    result $100 * + 
-    charUTF8 !
+    3 for
+        result 100 * to result
+        100 u/mod swap +to result
+    next
+    drop
+    result
   ;
 
-\ display UTF8 encoded character
+\ emit UTF8 encoded character
 : uemit ( n -- )
-    charUTF8 4 0 fill           \ reset charUTF8
-    reverse-bytes-to-charUTF8   \ reverse bytes
-    charUTF8 4 type             \ display UTF8 character
+    reverse-bytes
+    >r rp@ 4 type
+    rdrop
   ;
 
-安
+\ example:
+\ hex  E282AC uemit    \ display: €
 
-: tt s" 安" ;
 
 
